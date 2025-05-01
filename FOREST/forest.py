64140b9a63758ad05922e08007784d5f9115050c
Natalia <npieczko23@gmail.com>
2025-05-01 20:20:38 +0200

    Implement RandomForest and forest_study draft

--- FOREST/forest.py
+++ FOREST/forest.py
@@ -1,161 +1,58 @@
-from ID3.build_tree import build_tree
-from ID3.predict import predict
-from SVM.svm import train_svm, predict_SVM
+from ID3.ID3 import ID3
 import pandas as pd
 import random
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
-# from sklearn.model_selection import train_test_split
-# from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from scipy.stats import mode
 
 class RandomForest:
-    def __init__(self, num_ID3, num_SVMS, regularisation: int, kernel: str, degree: int, gamma:str, data: pd.DataFrame):
-
-        self._svm_data = data.__deepcopy__()
-        self._id3_data = data.__deepcopy__()
-
-        
-
-
-        categorical_columns = []
-        for label, content in list(self._svm_data.items())[:-1]:
-            if pd.api.types.is_object_dtype(content):
-                categorical_columns.append(label)
-
-        
-        if len(categorical_columns) != 0:
-            svm_cat = pd.get_dummies(self._svm_data[categorical_columns])
-            self._svm_data.drop(columns=categorical_columns, inplace=True)
-            self._svm_data = svm_cat.join(self._svm_data)
-
-            for column in categorical_columns:
-                le = LabelEncoder()
-                self._id3_data[column] = le.fit_transform(self._id3_data[column])
-
-
-        self.encoder = LabelEncoder()
-        last_col = self._svm_data.iloc[:, -1]
-        self.encoder.fit(last_col)
-        transformed_col = self.encoder.transform(last_col)
-
-        self._id3_data.iloc[:, -1] = transformed_col
-    
-
-    
+    def __init__(self, num_ID3: int, num_SVM: int, id3_max_depth: int = 10, svm_regularization: float = 1.0, svm_kernel: str = 'rbf'):
         self.num_ID3 = num_ID3
-        self.num_SVMS = num_SVMS
-        self.regularisation = regularisation
-        self.kernel = kernel
-        self.degree = degree
-        self.gamma = gamma
-        self.data = data
-        self.ID3_models = []
-        self.SVM_models = []
+        self.num_SVM = num_SVM
+        self.svm_regularization = svm_regularization
+        self.svm_kernel = svm_kernel
+        self.id3_max_depth = id3_max_depth
+        self.ID3_models = [ID3(self.id3_max_depth) for _ in range(num_ID3)]
+        self.SVM_models = [SVC(C=self.svm_regularization, kernel=self.svm_kernel) for _ in range(num_SVM)]
 
-        self.target_name = data.columns[-1]
-        self.feature_names = list(data.columns[:-1])
+        self.encoded_columns_ = None
 
-        self.subset_len = max(1, int(np.floor(np.sqrt(len(self.feature_names)))))
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        X.reset_index(drop=True, inplace=True)
+        feature_names = list(X.columns)
+        feature_names_subset_len = max(1, int(np.floor(np.sqrt(len(feature_names)))))
 
-    def train(self):
-        for i in range(self.num_ID3):
-            features_subset = random.sample(self.feature_names, self.subset_len)
-            bootstrap_sample = self._id3_data.sample(n=len(self.data), replace=True, random_state=i)
+        for ID3_model in self.ID3_models:
+            features_names_subset = random.sample(feature_names, feature_names_subset_len)
+            X_subset = X[features_names_subset]
 
-            model = build_tree(bootstrap_sample, features_subset, self.data.columns[-1])
-            self.ID3_models.append(model)
+            bootstrap_ids = np.random.randint(0, X_subset.shape[0], size=X_subset.shape[0])
+            X_bootstraped = X_subset.iloc[bootstrap_ids]
+            y_bootstraped = y.iloc[bootstrap_ids]
 
-        for i in range(self.num_SVMS):
-            bootstrap_sample = self._svm_data.sample(n=len(self.data), replace=True, random_state=self.num_ID3 + i)
+            ID3_model.fit(X_bootstraped, y_bootstraped)
 
+        for SVM_model in self.SVM_models:
+            bootstrap_ids = np.random.randint(0, X.shape[0], size=X.shape[0])
+            X_bootstraped = X.iloc[bootstrap_ids]
+            y_bootstraped = y.iloc[bootstrap_ids]
 
-            model = train_svm(bootstrap_sample, self.regularisation, self.kernel, self.degree, self.gamma)
-            self.SVM_models.append(model)
+            SVM_model.fit(X_bootstraped, y_bootstraped)
 
     def predict(self, X: pd.DataFrame):
-        X_id3 = X.__deepcopy__()
-        X_svm = X.__deepcopy__()  
-
-        if self._svm_data.isna().any().any():
-            print("Warning: Input data contains NaN values. Imputing missing values...")
-
-        categorical_columns = []
-        for label, content in  list(X.items()):
-            if pd.api.types.is_object_dtype(content):
-                categorical_columns.append(label)
-
-        if len(categorical_columns) != 0:
-
-            svm_cat = pd.get_dummies( X_svm[categorical_columns])
-            X_svm.drop(columns=categorical_columns, inplace=True)
-            X_svm = svm_cat.join(X_svm)
-
-            for column in categorical_columns:
-                le = LabelEncoder()
-                X_id3[column] = le.fit_transform(X[column])
-        
-        if self._svm_data.isna().any().any():
-            print("Warning: Input data contains NaN values. Imputing missing values...")
         predictions = []
-        for model in self.ID3_models:
-            pred = predict(model, X_id3)
-            predictions.append(pred)
 
-        for model in self.SVM_models:
-            pred = predict_SVM(model, X_svm)
-            # pred = predict_SVM(model, X_id3)
-            predictions.append(self.encoder.transform(pred))
-        return predictions
+        for ID3_model in self.ID3_models:
+            prediction = ID3_model.predict(X)
+            predictions.append(prediction)
 
-    def predict_majority_vote(self, X: pd.DataFrame):
-        predictions = self.predict(X)
-        final_predictions = []
-        for i in range(len(X)):
-            votes = {}
-            for pred in predictions:
-                if pred[i] not in votes:
-                    votes[pred[i]] = 1
-                else:
-                    votes[pred[i]] += 1
-            final_predictions.append(max(votes, key=votes.get))
-        return final_predictions
+        for SVM_model in self.SVM_models:
+            prediction = SVM_model.predict(X)
+            predictions.append(prediction)
 
 
-# mean1 = 55
-# std_dev1 = 10
-# num_samples = 500
+        all_preds_array = np.array(predictions)
+        all_preds_per_sample = all_preds_array.T
+        majority_votes, _ = mode(all_preds_per_sample, axis=1, keepdims=False)
 
-# column1_numbers = np.random.normal(mean1, std_dev1, num_samples)
-# column1_numbers = np.clip(column1_numbers, 30, 120)
-# column1_numbers = np.round(column1_numbers).astype(int)
-
-# mean2 = 18
-# std_dev2 = 3
-
-# column2_numbers = np.random.normal(mean2, std_dev2, num_samples)
-# column2_numbers = np.clip(column2_numbers, 12, 26)
-# column2_numbers = np.round(column2_numbers).astype(int)
-
-# column3_numbers = np.random.randint(2, size=num_samples)
-# column3_numbers[column1_numbers > mean1] =1
-
-# data = {"Miles_Per_week": column1_numbers,
-#         "Farthest_run": column2_numbers,
-#         "Qualified_Boston_Marathon": column3_numbers}
-
-# df = pd.DataFrame(data)
-
-# train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
-
-# # Train the model
-# forest = RandomForrest(num_ID3=1, num_SVMS=1, regularisation=1, kernel='rbf', degree=3, gamma='scale', data=train_df)
-# forest.train()
-
-
-# X_test = test_df.iloc[:, 0:len(test_df.columns)-1]
-# y_test = test_df.iloc[:, len(test_df.columns)-1]
-# # Predict using the model
-# predictions = forest.predict_majority_vote(X_test)
-# # Calculate accuracy
-# accuracy = accuracy_score(y_test, predictions)
-# print(f"Test Accuracy: {accuracy:.2f}")
+        return majority_votes
--- forest_study.py
+++ forest_study.py
@@ -5,69 +5,88 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 
 def main():
+
+    # -- MUSHROOMS --
     mushrooms_df = pd.read_csv("DATA/mushrooms.csv")
-    wine_df = pd.read_csv("DATA/WineQT.csv")
-    cancer_df = pd.read_csv("DATA/data.csv")
+    mushrooms_df = mushrooms_df.iloc[:, ::-1]
 
+    target_column_name = mushrooms_df.columns[-1]
+    feature_column_names = list(mushrooms_df.columns[:-1])
 
-    mushrooms_df =mushrooms_df.iloc[:, ::-1]
-    wine_df = wine_df.iloc[:, :-1]
-    cancer_df = cancer_df.drop(columns=['id', 'Unnamed: 32'], errors='ignore')
-    cancer_df = cancer_df.iloc[:, ::-1]
+    X = mushrooms_df[feature_column_names]
+    y = mushrooms_df[target_column_name]
 
-    mushroom_train_df, mushroom_test_df = train_test_split(mushrooms_df, test_size=0.2, random_state=42)
+    categorical_columns = list(X.select_dtypes(include=("object","category")).columns)
+    X = pd.get_dummies(X, columns=categorical_columns)
+
+    X_mushroom_train_df, X_mushroom_test_df, y_mushroom_train_df, y_mushroom_test_df = train_test_split(X, y, test_size=0.2, random_state=42)
     encoder = LabelEncoder()
 
-    X_test = mushroom_test_df.iloc[:, 0:len(mushroom_test_df.columns)-1]
-    y_test = mushroom_test_df.iloc[:, len(mushroom_test_df.columns)-1]
+    y_mushroom_train_df = pd.Series(encoder.fit_transform(y_mushroom_train_df))
+    y_mushroom_test_df = pd.Series(encoder.transform(y_mushroom_test_df))
 
-    y_test = encoder.fit_transform(y_test)
+    forest = RandomForest(num_ID3=10, num_SVM=10)
+    forest.fit(X_mushroom_train_df, y_mushroom_train_df)
 
-    forest = RandomForest(num_ID3=10, num_SVMS=10, regularisation=1, kernel='rbf', degree=3, gamma='scale', data=mushroom_train_df)
-    forest.train()
-
-    predictions = forest.predict_majority_vote(X_test)
-    accuracy = accuracy_score(y_test, predictions)
+    predictions = forest.predict(X_mushroom_test_df)
+    accuracy = accuracy_score(y_mushroom_test_df, predictions)
     print(f"Mushroom Test Accuracy: {accuracy:.2f}")
 
+    # -- WINE --
+    wine_df = pd.read_csv("DATA/WineQT.csv")
+    wine_df = wine_df.iloc[:, :-1]
 
     wine_train_df, wine_test_df = train_test_split(wine_df, test_size=0.2, random_state=42)
     encoder = LabelEncoder()
 
-    X_test = wine_test_df.iloc[:, 0:len(wine_test_df.columns)-1]
-    y_test = wine_test_df.iloc[:, len(wine_test_df.columns)-1]
+    target_column_name = wine_train_df.columns[-1]
+    feature_column_names = list(wine_train_df.columns[:-1])
 
-    y_test = encoder.fit_transform(y_test)
+    X_train = wine_train_df[feature_column_names]
+    y_train = wine_train_df[target_column_name]
+    X_test = wine_test_df[feature_column_names]
+    y_test = wine_test_df[target_column_name]
 
-    forest = RandomForest(num_ID3=10, num_SVMS=10, regularisation=1, kernel='rbf', degree=3, gamma='scale', data=wine_train_df)
-    forest.train()
+    y_train = pd.Series(encoder.fit_transform(y_train))
+    y_test = pd.Series(encoder.transform(y_test))
 
-    predictions = forest.predict_majority_vote(X_test)
+    forest = RandomForest(num_ID3=10, num_SVM=10)
+    forest.fit(X_train, y_train)
+
+    predictions = forest.predict(X_test)
     accuracy = accuracy_score(y_test, predictions)
-    print(f"Wine Test Accuracy: {accuracy:.2f}")
+    print(f"Wine Test Accuracy Label: {accuracy:.2f}")
 
 
+    # -- CANCER --
+    cancer_df = pd.read_csv("DATA/data.csv")
+
+    cancer_df = cancer_df.drop(columns=['id', 'Unnamed: 32'], errors='ignore')
+    cancer_df = cancer_df.iloc[:, ::-1]
 
     cancer_train_df, cancer_test_df = train_test_split(cancer_df, test_size=0.2, random_state=42)
     encoder = LabelEncoder()
 
-    X_test = cancer_test_df.iloc[:, 0:len(cancer_test_df.columns)-1]
-    y_test = cancer_test_df.iloc[:, len(cancer_test_df.columns)-1]
+    target_column_name = cancer_train_df.columns[-1]
+    feature_column_names = list(cancer_train_df.columns[:-1])
+
+    X_train = cancer_train_df[feature_column_names]
+    y_train = cancer_train_df[target_column_name]
+    X_test = cancer_test_df[feature_column_names]
+    y_test = cancer_test_df[target_column_name]
+
+    y_train = pd.Series(encoder.fit_transform(y_train))
+    y_test = pd.Series(encoder.transform(y_test))
 
     y_test = encoder.fit_transform(y_test)
 
-    forest = RandomForest(num_ID3=10, num_SVMS=10, regularisation=1, kernel='rbf', degree=3, gamma='scale', data=cancer_train_df)
-    forest.train()
+    forest = RandomForest(num_ID3=10, num_SVM=10)
+    forest.fit(X_train, y_train)
 
-    predictions = forest.predict_majority_vote(X_test)
+    predictions = forest.predict(X_test)
     accuracy = accuracy_score(y_test, predictions)
     print(f"Cancer Test Accuracy: {accuracy:.2f}")
-
-    
-
-
 
 
 if __name__ == "__main__":
     main()
-  
